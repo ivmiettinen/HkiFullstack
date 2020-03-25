4589115a65abca41aefb3918c1191f1d1d5fcd7e
Ilkka Miettinen <ivmiettinen@gmail.com>
2020-03-25 15:40:09 +0200

    anecdote tasks

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,55 +1,61 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-// let Jono = Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0);
+const points = Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0);
 
-// console.log('jono:', Jono);
-
-const points = [0, 0, 0, 0, 0, 0];
-
-const alkuperaisenKopio = [...points];
-
-const App = props => {
-  const [selected, setSelected] = useState([
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]);
-  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(0);
 
   const handleRandomVote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
 
-  const maksimi = Math.max(...alkuperaisenKopio);
+  console.log('pituus', setSelected.length);
 
-  const missakohtaa = alkuperaisenKopio.indexOf(maksimi);
+  const maximumPoints = Math.max(...points);
 
-  console.log('missaKohtaa', missakohtaa);
+  const popularAnecdote = points.indexOf(maximumPoints);
+
+  let copyOfAnecdotes = [...anecdotes];
 
   const handleVote = () => {
-    // setVotes((JonoKopio[selected] += 1));
+    points[selected] += 1;
 
-    setVotes(alkuperaisenKopio.concat(+0));
-
-    alkuperaisenKopio[selected] += 1;
-
-    console.log('votes state:', alkuperaisenKopio[selected]);
+    setVotes({ ...points });
   };
 
-  console.log('votes:', votes);
+  if (votes === 0) {
+    return (
+      <div>
+        <h3>Anecdote of the day:</h3>
+        <p>"{anecdotes[selected]}"</p>
+        <p>has 0 votes.</p>
+        <button onClick={handleRandomVote}>Next Anecdote</button>
+        <p></p>
+        <button onClick={handleVote}>Vote</button>
+
+        <h3>Anecdote with most votes: </h3>
+        <p>
+          "{copyOfAnecdotes[popularAnecdote]}" has {maximumPoints} votes.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
+      <h3>Anecdote of the day:</h3>
+      <p>"{anecdotes[selected]}"</p>
+      <p>has {votes[selected]} votes.</p>
       <button onClick={handleRandomVote}>Next Anecdote</button>
-      {props.anecdotes[selected]}
+      <p></p>
       <button onClick={handleVote}>Vote</button>
-      <p> {alkuperaisenKopio[selected]}</p>
-      <p>Votes:{votes[selected]} statesta </p>
-      <p>{alkuperaisenKopio[selected]} votes.</p>
+
+      <h3>Anecdote with most votes: </h3>
+      <p>
+        "{copyOfAnecdotes[popularAnecdote]}" has {maximumPoints} votes.
+      </p>
     </div>
   );
 };
