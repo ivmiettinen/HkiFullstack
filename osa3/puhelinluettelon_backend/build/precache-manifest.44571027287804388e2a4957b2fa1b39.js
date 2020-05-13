cb0b27ea5475b545672ce040380cc4bae91cddad
Ilkka <ivmiettinen@gmail.com>
2020-05-13 12:06:08 +0300

    puhelinluettelon backend

---
+++ osa3/puhelinluettelon_backend/build/precache-manifest.44571027287804388e2a4957b2fa1b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3642a9002ed87e05f82e3e29055adb21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9d02063de4bb46c637",
+    "url": "/static/css/main.70d2331a.chunk.css"
+  },
+  {
+    "revision": "7ec50a7f66db804bb8ff",
+    "url": "/static/js/2.c3652788.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3652788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9d02063de4bb46c637",
+    "url": "/static/js/main.045505b7.chunk.js"
+  },
+  {
+    "revision": "d3dff64eb29962849d1e",
+    "url": "/static/js/runtime-main.019e163a.js"
+  }
+]);
