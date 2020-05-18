be0a34c9305c2e848830dc6c4a9103b86414f910
Ilkka <ivmiettinen@gmail.com>
2020-05-18 11:56:40 +0300

    Added Eslint file and fixed style errors

--- osa3/puhelinluettelo_ja_tietokanta/models/person.js
+++ osa3/puhelinluettelo_ja_tietokanta/models/person.js
@@ -1,38 +1,38 @@
-const mongoose = require("mongoose");
-mongoose.set("useFindAndModify", false);
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 
-const uniqueValidator = require("mongoose-unique-validator");
+const uniqueValidator = require('mongoose-unique-validator')
 
 
 
 // const password = process.argv[2];
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: true,
-    unique: true
-  },
-  number: {
-    type: String,
-    required: true,
-    minlength: 8
-  },
-});
+    name: {
+        type: String,
+        minlength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+})
 
 
 //Apply the uniqueValidator plugin to userSchema:
@@ -40,12 +40,12 @@ const personSchema = new mongoose.Schema({
 personSchema.plugin(uniqueValidator)
 
 
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
