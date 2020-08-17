5dfd0109a9eca86f65fe8a99dc608b8c491a239b
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2020-08-17 14:49:26 +0300

    tasks 4.11 & 4.12

--- osa4/Blog_list_tests_step1/controllers/blogs.js
+++ osa4/Blog_list_tests_step1/controllers/blogs.js
@@ -23,17 +23,30 @@ notesRouter.get('/', async (request, response, next) => {
 //         })
 // })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then((note) => {
-            if (note) {
-                response.json(note.toJSON())
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch((error) => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+    try {
+        const blogs = await Blog.findById(request.params.id)
+
+        if (blogs) {
+            response.json(blogs.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
 })
+// notesRouter.get('/:id', (request, response, next) => {
+//     Blog.findById(request.params.id)
+//         .then((note) => {
+//             if (note) {
+//                 response.json(note.toJSON())
+//             } else {
+//                 response.status(404).end()
+//             }
+//         })
+//         .catch((error) => next(error))
+// })
 
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
@@ -50,7 +63,7 @@ notesRouter.post('/', async (request, response, next) => {
             const savedNote = await note.save()
             response.json(savedNote.toJSON())
         } else {
-            console.log('tilte ja/tai url puuttuvat ', note)
+            console.log('title ja/tai url puuttuvat ', note)
             response.status(400).send('Bad request')
         }
     } catch (exception) {
@@ -91,19 +104,75 @@ notesRouter.delete('/:id', async (request, response, next) => {
 //         .catch((error) => next(error))
 // })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response, next) => {
     const body = request.body
 
     const note = {
-        content: body.content,
-        important: body.important,
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
     }
 
-    Blog.findByIdAndUpdate(request.params.id, note, { new: true })
-        .then((updatedNote) => {
-            response.json(updatedNote.toJSON())
+    try {
+        const blogs = await Blog.findByIdAndUpdate(request.params.id, note, {
+            new: true,
         })
-        .catch((error) => next(error))
+
+        response.json(blogs.toJSON())
+    } catch (exception) {
+        console.log('exception', exception)
+        next(exception)
+    }
 })
+
+//     Blog.findByIdAndUpdate(request.params.id, note, { new: true })
+//         .then((updatedNote) => {
+//             console.log('updatedNote', updatedNote)
+//             response.json(updatedNote.toJSON())
+//         })
+//         .catch((error) => next(error))
+// })
+//
+// notesRouter.get('/', async (request, response, next) => {
+//     try {
+//         const blogs = await Blog.find({})
+//         if (blogs) {
+//             response.json(blogs.map((allBlogs) => allBlogs.toJSON()))
+//         } else {
+//             response.status(404).end()
+//         }
+//     } catch (exception) {
+//         next(exception)
+//     }
+// })
+//////////////////////////////////////////////////////////////
+
+// notesRouter.put('/:id', (request, response, next) => {
+//     const body = request.body
+
+//     const note = {
+//         title: body.title,
+//         author: body.author,
+//         url: body.url,
+//         likes: body.likes,
+//     }
+
+//     //
+//     // id: Number,
+//     // title: String,
+//     // author: String,
+//     // url: String,
+//     // likes: Number
+
+//     //
+
+//     Blog.findByIdAndUpdate(request.params.id, note, { new: true })
+//         .then((updatedNote) => {
+//             console.log('updatedNote', updatedNote)
+//             response.json(updatedNote.toJSON())
+//         })
+//         .catch((error) => next(error))
+// })
 
 module.exports = notesRouter
